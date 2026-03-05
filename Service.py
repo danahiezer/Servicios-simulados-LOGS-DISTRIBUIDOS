5de254b50fb73312dfb396b9fab28c1528849214
danahiezer <dansantacruz5@gmail.com>
2026-03-05 18:09:08 -0300

    funcion para enviar esos logs al servidor

--- Service.py
+++ Service.py
@@ -2,21 +2,34 @@ import requests
 from datetime import datetime,timezone
 import random
 
-
+# lista de servicios, con nombres del servicio y tokens
 servicios = [
     {"nombre": "auth-service",      "token": "token-autenticacion"},
     {"nombre": "payment-service",   "token": "token-de-pago"},
     {"nombre": "inventario-service","token": "token-de-inventario"},
 ]
 
-
+# funcion para generar esos logs random
 def generaLogs():
     service = random.choice(servicios)
     logs = {
-        "timestamp": datetime.now(timezone.utc).isoformat(),
+        "timestamp": datetime.now(timezone.utc).isoformat(), # --> obtiene el tiempo con la zona horaria y lo  muestra en formato iso
         "service": service["nombre"],
         "severity": random.choice(["ERROR","WARNING","INFO","DEBUG"]),
         "message": "cualquiera"
     }
 
-    return logs, servicios["token"]
+    return logs, service["token"]
+# funcion para enviar los logs randoms
+def enviarLogs(log,token):
+    headers = {
+        "Authorization": f"Token {token}" # --> header con el token 
+    }
+
+    respuesta = requests.post(
+        "http://localhost:8000/logs", # --> respuesta (se envia el log) con la url el json del log y su header
+        json = {"logs": [log]},
+        headers = headers
+    )
+
+    print(f"status: {respuesta.status_code}") # --> muestra el estado de la respuesta 
